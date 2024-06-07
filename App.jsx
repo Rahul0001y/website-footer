a8f312d49a076e33f8e2946ddffa3384dfc0488e
Rahul001y <Rahul00jrs@gmail.com>
2024-06-07 11:49:31 +0530

    rectsetup

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div class name="mainfooter">
+     <div class name="firstfooter"><h1>Laxmi Devi Institute of Enginneering college</h1></div>
+     <div class name="secondfooter"><h1>Quick Link</h1></div>
+     <div class name="thirdfooter">Courses</div>
+     <div class name="fourthfooter">External Link</div>
+     </div>
+
+        
+    </>
+  )
+}
+
+export default App
